61e21c486d58b92147350d4daa9238d08fc4cff2
Eivlys <vamcheryl@gmail.com>
2014-09-24 23:14:27 +0800

    make some change
    didn’t work out

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,8 +5,5 @@
         //open outgoing links in new window
         $('a[href^="http://"]').add('a[href^="https://"]').attr('target','_blank');
     });
-<<<<<<< HEAD
+
 }(jQuery));
-=======
-}(jQuery));
->>>>>>> FETCH_HEAD
